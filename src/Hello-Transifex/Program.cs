3daf4721043370e42b193f8b9b04b7b5bde8e186
AdmiringWorm <kim.nordmo@gmail.com>
2017-03-16 07:40:18 +0100

    Add project files.

---
+++ src/Hello-Transifex/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Hello_Transifex
+{
+    using System;
+    using Hello_Transifex.Properties;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Resources.HelloWorld);
+            Console.WriteLine();
+            Console.WriteLine(Resources.SeeTheWiki);
+            Console.WriteLine();
+            Console.WriteLine(Resources.ThankYou);
+
+            Console.ReadLine();
+        }
+    }
+}
